3bf9bef402e626ec80a664903204fc6fcd12ffc7
muradbdvc <muradbdvc@gmail.com>
2026-01-07 23:16:00 +0600

    Nav menu transfer to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,24 +5,28 @@ import Hero from "./components/hero/Hero";
 import Testimonial from "./components/testimonial/Testimonial";
 import Pricing from "./components/pricing/Pricing";
 import Featured from "./components/featured/Featured";
-import AboutUs from "./components/aboutus/AboutUs";
+import About from "./components/pages/About";
 import Footer from "./components/footer/Footer";
+import Home from "./components/pages/Home";
+import Work from "./components/pages/Work";
+import Contact from "./components/pages/Contact";
 
 function App() {
   return (
     <div className="min-h-screen overflow-hidden text-white scroll-auto">
       <header>
         <section className="bg-violet">
-          <div className="pt-8 text-white">
-            {/*Logo*/}
-            <Link
-              to="/"
-              className="text-3xl beautique font-light absolute py-2"
-            >
-              Veauly
-            </Link>
-          </div>
           <BrowserRouter>
+            <div className="pt-8 text-white">
+              {/*Logo*/}
+              <Link
+                to="/"
+                className="text-3xl beautique font-light absolute py-2"
+              >
+                Veauly
+              </Link>
+            </div>
+
             {/** Mobile Menu */}
             <nav className="flex justify-center mx-auto absolute right-0 top-0 left-0 pt-8">
               <ul className="flex flex-raw space-x-10 rounded-4xl pl-5 border-2 border-White-lite">
@@ -46,7 +50,7 @@ function App() {
       <Featured />
       <Testimonial />
       <Pricing />
-      <AboutUs />
+      <About />
       <Footer />
       <p className="m-9">Hello</p>
     </div>
