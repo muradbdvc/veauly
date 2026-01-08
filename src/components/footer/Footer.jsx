21331ec3dc3dbc3461844bd7f9ea4e757505e175
muradbdvc <muradbdvc@gmail.com>
2026-01-08 23:53:52 +0600

    footer text color

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,14 +5,16 @@ const Footer = () => {
   return (
     <>
       <div className="footer h-screen content-center mx-auto">
-        <h1>Let's Talk</h1>
+        <h1 className="mx-auto">Let's Talk</h1>
         <button className="bg-black text-white px-6 py-2 rounded-full hover:bg-white hover:text-black border border-black transition-all duration-300 font-outfit uppercase text-xs tracking-widest">
           View Project
         </button>
       </div>
       <section>
         <div className="fixed-footer py-36 mx-16">
-          <h1 className="text-3xl text-black">Veauly Studio</h1>
+          <h1 className="text-3xl text-black footer-brand-name">
+            Veauly Studio
+          </h1>
         </div>
       </section>
     </>
